51bab35de972749f1e6bdffe68b49be578a86e53
Hugh <hughglynparry@gmail.com>
2024-04-16 16:21:54 +0100

    refactoring geoip_2 logic

--- src/main.rs
+++ src/main.rs
@@ -22,11 +22,34 @@ async fn main() {
     let ip_str = matches.get_one::<String>("IP").expect("IP argument missing");
     let ip: IpAddr = ip_str.parse().expect("Invalid IP address");
 
-    let response = get_geolocation_data(&ip).await.unwrap_or_else(|_| {
-        eprintln!("Failed to retrieve geolocation data");
-        std::process::exit(1);
-    });
+    match get_geoip2_data(&ip).await {
+        Ok(response) => print_geoip2_data(&response),
+        Err(e) => {
+            eprintln!("Failed to retrieve geolocation data: {}", e);
+            std::process::exit(1);
+        }
+    }
+}
 
+
+async fn get_geoip2_data(ip: &IpAddr) -> Result<Value, reqwest::Error> {
+    let account_id = env::var("GEOIP2_ACCOUNT_ID").expect("GEOIP2_ACCOUNT_ID not set in .env file");
+    let license_key = env::var("GEOIP2_LICENSE_KEY").expect("GEOIP2_LICENSE_KEY not set in .env file");
+
+    let url = format!("https://geolite.info/geoip/v2.1/city/{}?pretty", ip);
+    let client = reqwest::Client::new();
+
+    client
+        .get(url)
+        .basic_auth(account_id, Some(license_key))
+        .send()
+        .await?
+        .json::<Value>()
+        .await
+}
+
+
+fn print_geoip2_data(response: &Value) {
     if let Some(city) = response["city"]["names"]["en"].as_str() {
         println!("City: {}", city);
     }
@@ -48,20 +71,3 @@ async fn main() {
     }
 }
 
-async fn get_geolocation_data(ip: &IpAddr) -> Result<Value, reqwest::Error> {
-    let account_id = env::var("GEOIP2_ACCOUNT_ID").expect("GEOIP2_ACCOUNT_ID not set in .env file");
-    let license_key = env::var("GEOIP2_LICENSE_KEY").expect("GEOIP2_LICENSE_KEY not set in .env file");
-
-    let url = format!("https://geolite.info/geoip/v2.1/city/{}?pretty", ip);
-    let client = reqwest::Client::new();
-
-    let res = client
-        .get(url)
-        .basic_auth(account_id, Some(license_key))
-        .send()
-        .await?
-        .json::<Value>()
-        .await;
-
-    res
-}
